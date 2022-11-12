78f84313428d3628a7103b9b8296d0f9526a5da9
Jordan <100964858+j0j032@users.noreply.github.com>
2022-11-12 12:28:21 +0100

    ✨ select query limit employees per page

--- src/api/employees/requests.js
+++ src/api/employees/requests.js
@@ -2,4 +2,4 @@ import axios from 'axios'
 
 const employeesURL = 'http://localhost:3500/employees'
 
-export const getAllEmployees = async () => axios.get(employeesURL).then(r => r.data)
+export const getAllEmployees = async (page, limit) => axios.get(`${employeesURL}?p=${page}&limit=${limit}`).then(r => r.data)
--- src/modules/views/Employees/EmployeesGallery/EmployeesGallery.jsx
+++ src/modules/views/Employees/EmployeesGallery/EmployeesGallery.jsx
@@ -1,13 +1,40 @@
 import {useGetAllEmployees} from '../../../../api/employees/useGetEmployees.js'
 import {EmployeeCard} from '../EmployeeCard/EmployeeCard'
+import {useState} from 'react'
+
+const options = [
+	{value: '12', text: '12'},
+	{value: '24', text: '24'},
+	{value: '36', text: '36'},
+	{value: '48', text: '48'},
+	{value: '50', text: '50'}
+]
 
 export const EmployeesGallery = () => {
-	const {data: employees, isLoading, isFetched} = useGetAllEmployees({enabled: true})
+	const [page, setPage] = useState(0)
+	const [limit, setLimit] = useState(12)
+	const {data, isLoading, isFetched} = useGetAllEmployees(page, limit, {enabled: true})
+	const [selected, setSelected] = useState(options[0].value)
+	
+	const handleChangeLimit = e => {
+		setSelected(e.target.value)
+		setLimit(e.target.value)
+	}
+	
+	const numberOfPages = () => {
+		if (isLoading) {
+			return 'LOADING'
+		}
+		if (isFetched) {
+			return (data.employeesLength / limit)
+		}
+	}
+	
 	
 	return isLoading ? <div>LOADING</div> : (
 		<div className='emp-gallery__container'>
 			<div className='emp-gallery__wrapper'>
-				{isFetched && employees.map((employee) => <EmployeeCard
+				{isFetched && data.employees.map((employee) => <EmployeeCard
 					key={employee._id}
 					hired={employee.hired}
 					picture={employee.picture}
@@ -19,10 +46,21 @@ export const EmployeesGallery = () => {
 					address={employee.address}
 				/>)}
 			</div>
+			
 			<div className='emp-gallery__pagination'>
+				<div className='emp-gallery__pagination--limit'>
+					<p>Show </p>
+					<select value={selected} onChange={handleChangeLimit}>
+						{options.map(option => (<option key={option.value}>{option.text}</option>))}
+					</select>
+					<p>employees per page</p>
+				</div>
 				<p className='emp-gallery__result'>
-					<span>{employees.length}</span>{employees.length > 1 ? ' Employees' : ' Employee'}
+					<span>{data.employeesLength}</span>{data.employeesLength > 1 ? ' Employees' : ' Employee'}
 				</p>
+				<div>
+					{numberOfPages()}
+				</div>
 			</div>
 		</div>
 	)
