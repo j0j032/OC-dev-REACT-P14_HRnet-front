a49d933c1c86a76be2178ddec716333598f2fef5
Jordan <100964858+j0j032@users.noreply.github.com>
2022-11-16 09:37:50 +0100

    ✨ Introduce 404 page

--- src/modules/views/Error404/Error404.jsx
+++ src/modules/views/Error404/Error404.jsx
@@ -1,9 +1,30 @@
+import {DarkMode} from '../../components/DarkMode/DarkMode'
+import {useNavigate} from 'react-router-dom'
+
 export const Error404 = () => {
+	const navigate = useNavigate()
 	return (
 		<>
-			<div>
-				Error404
-			</div>
+			<header className='error-404__header'>
+				<div className='header__logo'></div>
+				<DarkMode/>
+			</header>
+			<section className='error-404__content'>
+				<div className='error-404__content-layer'>
+					<h1>404</h1>
+					<div className='error-404__content-layer2'>
+						<p>
+							Sorry, <br/>
+							👨‍🚀 You're lost <br/>
+							The Location couldn't be found..<br/><br/>
+							👀 If you're trying to access another page <br/>
+							from the menu ! keep in mind.. this web app is fake <br/>
+							The only page and functionalities you can access is: Employees
+						</p>
+						<button onClick={() => navigate('/employees')}>Go Back</button>
+					</div>
+				</div>
+			</section>
 		</>
 	)
 }
