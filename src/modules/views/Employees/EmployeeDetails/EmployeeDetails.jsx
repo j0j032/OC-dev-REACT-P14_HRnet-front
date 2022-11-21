8f45219aa5f0dfd86dbb150653019f3e3cacfb4f
Jordan <100964858+j0j032@users.noreply.github.com>
2022-11-21 15:24:50 +0100

    ✨ add employee modal content

--- src/modules/views/Employees/EmployeeDetails/EmployeeDetails.jsx
+++ src/modules/views/Employees/EmployeeDetails/EmployeeDetails.jsx
@@ -2,23 +2,46 @@ import {Loader} from '../../../components/Loader/Loader.jsx'
 import {useQuery} from 'react-query'
 import {getEmployeeById} from '../../../../api/employees/requests.js'
 import React from 'react'
+import {Error} from '../../../components/Error/Error'
+import {formatToLocale} from '../../../../utils/dateFormater'
+import editIcon from '../../../../assets/icons/edit.svg'
+import deleteIcon from '../../../../assets/icons/delete.svg'
+import sendIcon from '../../../../assets/icons/send.svg'
 
 export const EmployeeDetails = ({id}) => {
-	const {data, isLoading, isError} = useQuery(['employee'], () => getEmployeeById(id), {
+	const {data: user} = useQuery(['login'], {enabled: false}), {userInfos} = user, {company} = userInfos
+	const {data, isLoading, error, isError} = useQuery(['employee'], () => getEmployeeById(id), {
 		refetchOnWindowFocus: false
 	})
-	const {data: user} = useQuery(['login'], {enabled: false}), {userInfos} = user, {company} = userInfos
 	return (
 		<>
-			{isLoading ? <Loader/> : isError ? <div>ERROR</div> : (
+			{isLoading ? <Loader/> : isError ? <Error message={error.message}/> : (
 				<div className='employee-details__container'>
 					<img src={data.picture} alt={`Profile picture of ${data.firstname}`}/>
 					<div className='employee-details__infos'>
-						<div className='employee-details__names'>
-							<h1>{data.firstname}</h1>
-							<h1>{data.lastname}</h1>
+						<div className='employee-details__heading'>
+							<div className='employee-details__names'>
+								<h1>{data.firstname}</h1>
+								<h1>{data.lastname}</h1>
+							</div>
+							<p> {`Joined ${company.name} : ${formatToLocale(data.hired, 'en-US')}`}</p>
 						</div>
-						<p>{`Joined ${company.name} : ${data.hired.slice(0, 10)}`}</p>
+						<h3> 💼 {data.title}</h3>
+						<p> 👫 {data.department} team</p>
+						<div className='employee-details__personal'>
+							<h3>Personal details:</h3>
+							<p>🎂 {formatToLocale(data.birthdate, 'en-US')}</p>
+							<p>📱 {data.contact.phone}</p>
+							<p>✉️ {data.contact.mail}</p>
+							<p>📫 Address:</p>
+							<p>{data.address.street}</p>
+							<p>{`${data.address.city} ${data.address.state} ${data.address.zip}`}</p>
+						</div>
+					</div>
+					<div className='employee-details__options'>
+						<img className='icon' src={sendIcon} alt={`Send message to ${data.firstname}`}/>
+						<img className='icon' src={editIcon} alt={`Edit ${data.firstname} profile`}/>
+						<img className='icon' src={deleteIcon} alt={`Delete ${data.firstname} profile`}/>
 					</div>
 					<div className='employee-details__company'>
 						<img src={company.logo} alt='Company logo'/>
