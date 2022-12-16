2c28df442d34404b74f694f51b2eb609b205188e
Jordan <100964858+j0j032@users.noreply.github.com>
2022-12-16 20:34:27 +0100

    ✨ Create sort btn cmpnt to sort employees on serverside

--- src/modules/views/Employees/components/EmployeesToolBar/EmployeesToolbar.jsx
+++ src/modules/views/Employees/components/EmployeesToolBar/EmployeesToolbar.jsx
@@ -1,52 +1,20 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext} from 'react'
 import {ViewContext} from '../../../../../context/EmpoyeesViewContext.jsx'
-import useWindowSize from '../../../../../hooks/useWindowSize.jsx'
 import useModal from '../../../../components/Modal/useModal.jsx'
 import Modal from '../../../../components/Modal/Modal.jsx'
 import {CreateEmployeeForm} from '../CreateEmployeeForm/CreateEmployeeForm'
-import {BsGrid3X3Gap, BsPlusLg, FiChevronDown, FiChevronUp, HiOutlineQueueList} from 'react-icons/all.js'
+import {BsGrid3X3Gap, BsPlusLg, HiOutlineQueueList} from 'react-icons/all.js'
+import {SortButton} from '../../../../components/common/SortButton.jsx'
+import {sortItems} from '../../../../../config/sortBtns.js'
 
-const sortItems = [
-	{sort: 'firstname', value: 'Firstname'},
-	{sort: 'lastname', value: 'Lastname'},
-	{sort: 'department', value: 'Department'},
-	{sort: 'address.state', value: 'State'},
-	{sort: 'address.city', value: 'City'},
-	{sort: 'address.zip', value: 'Zip'}
-]
-
-export function SortButton({inputName, sortName, setSort}) {
-	const [sortMethod, setSortMethod] = useState(1)
-	const sortValues = [[sortName, sortMethod]]
-	
-	const sortEmployees = (arr) => {
-		if (sortMethod === 1) {
-			setSortMethod(-1)
-		}
-		if (sortMethod === -1) {
-			setSortMethod(1)
-		}
-		const category = Object.fromEntries(arr)
-		setSort(category)
-	}
-	return (
-		<div>
-			<button className='sort-btn' onClick={() => sortEmployees(sortValues)}>
-				{inputName} {sortMethod === 1 ? (<FiChevronDown/>) : sortMethod === -1 ? (<FiChevronUp/>) : null}
-			</button>
-		</div>
-	)
-}
 
 const EmployeesToolbar = ({setSearch, setSort}) => {
-	const windowSize = useWindowSize()
 	const {toggleTableView, tableView} = useContext(ViewContext)
 	const [isOpenModal, {openModal, closeModal}] = useModal(false)
 	const handleChange = e => e.target.value.length >= 2 ? setSearch(e.target.value) : setSearch('')
 	const viewIcon = tableView
 		? <BsGrid3X3Gap className='icon-btn icon-btn--font icon-bigger'/>
 		: <HiOutlineQueueList className='icon-btn icon-btn--font icon-bigger'/>
-	
 	
 	return (
 		<>
@@ -75,4 +43,4 @@ const EmployeesToolbar = ({setSearch, setSort}) => {
 	)
 }
 
-export default React.memo(EmployeesToolbar)
+export default EmployeesToolbar
