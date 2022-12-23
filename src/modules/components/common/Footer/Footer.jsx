ff8fc82f494b5b930182116be3dda44619bbe519
Jordan <100964858+j0j032@users.noreply.github.com>
2022-12-23 09:31:10 +0100

    🍱 update assets: add smallLogo(favicon) => change path in Footer.jsx

--- src/modules/components/common/Footer/Footer.jsx
+++ src/modules/components/common/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 import useWindowSize from '../../../../hooks/useWindowSize.jsx'
 import logo from '../../../../assets/logoHrnetW.svg'
-import favicon from '../../../../../public/faviconHR.svg'
+import logoSmall from '../../../../assets/faviconHR.svg'
 import {smallerScreenNavConfig} from '../../../../config/breakPoints.js'
 
 export const Footer = () => {
 	const windowSize = useWindowSize()
 	return (
 		<footer className='nav-footer'>
-			{windowSize.width < smallerScreenNavConfig ? <img className='footer-small-logo' src={favicon} alt='logo'/> : <img src={logo} alt='logo'/>}
+			{windowSize.width < smallerScreenNavConfig ? <img className='footer-small-logo' src={logoSmall} alt='logo'/> : <img src={logo} alt='logo'/>}
 		</footer>
 	)
 }
