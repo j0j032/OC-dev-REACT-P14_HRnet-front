b5fe9263dc5023055035b9100cd201d2798a4997
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-02-16 15:32:43 +0100

    update api Urls for no-auth branch

--- src/api/employees.js
+++ src/api/employees.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {filterMainSearch} from '../utils/filters.js'
 import {useMutation, useQuery, useQueryClient} from 'react-query'
 
-const employeesURL = 'http://localhost:3500/employees'
+const employeesURL = 'http://localhost:3500/api/employees'
 
 const getEmployeeById = (id) => axios.get(`${employeesURL}/${id}`).then(r => r.data)
 
